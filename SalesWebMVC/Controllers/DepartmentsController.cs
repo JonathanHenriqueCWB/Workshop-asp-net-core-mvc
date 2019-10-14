53ed12317b869a16cac064ee1493c216ebad8965
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2019-10-14 00:41:37 -0300

    Firts model-controller-view - Departments

--- SalesWebMVC/Controllers/DepartmentsController.cs
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -13,7 +13,8 @@ namespace SalesWebMVC.Controllers
         {
             List<Department> list = new List<Department>();
             list.Add(new Department { Id = 1, Name = "Eletronics" });
-            list.Add(new Department { Id = 2, Name = "Fashion" });
+            list.Add(new Department { Id = 2, Name = "Programmer" });
+            list.Add(new Department { Id = 3, Name = "Developer" });
             return View(list);
         }
     }
