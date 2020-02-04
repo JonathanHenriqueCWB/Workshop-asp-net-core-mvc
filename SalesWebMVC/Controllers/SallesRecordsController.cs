78b93223f72620e3943974f6f9123d9963b1ab42
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2020-02-04 09:37:26 -0300

    Preparing sales search navigation views

---
+++ SalesWebMVC/Controllers/SallesRecordsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SallesRecordsController : Controller
+    {
+        #region Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region SimpleSearch
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        #endregion
+
+        #region GroupSearch
+        public IActionResult GroupSearch()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
